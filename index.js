7a2c25d96ba1e20455ccc03266ecff67a43c175b
Josh Rehman <josh@joshrehman.com>
2014-08-11 13:36:50 -0700

    Initial commit of recursive fibonacci function in node

---
+++ index.js
@@ -0,0 +1,10 @@
+//Calculate the nth fibonacci number
+function fib(n){
+	if (n === 0 || n === 1) return 1;
+	return fib(n-1) + fib(n-2);
+}
+
+//The command line interface to the function
+var arg = process.argv[2];
+var n = parseInt(arg);
+console.log('Fibonacci number:', n, fib(n));
